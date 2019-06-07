f61694c618a414adf26acd905abfbccc06f3dfac
michimani <michimani210@gmail.com>
2019-06-07 19:36:48 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,44 @@
+image-resizer
+===
+This is a simple script that resize image and out put some kinds of size image.
+
+# Runtime
+Python 3.x
+
+# Usage
+1. This scrpit uses Pillow library.
+
+```
+pip3 install Pillow
+```
+
+2. Create `resize_manifest.json`. In this file, set the size and name of the resized output.
+
+```
+cp resize_manifest.sample resize_manifest
+```
+
+3. Run this script.
+
+```
+python resize.py base_image.png
+```
+
+Resized images will created in `dist` directly with sub directly named UNIX timestamp.
+
+```
+./dist
+└── 1559903048
+    ├── android-chrome-192x192.png
+    ├── android-chrome-512x512.png
+    ├── apple-touch-icon-120x120.png
+    ├── apple-touch-icon-152x152.png
+    ├── apple-touch-icon-180x180.png
+    ├── apple-touch-icon-180x76.png
+    ├── apple-touch-icon-60x60.png
+    ├── apple-touch-icon.png
+    ├── favicon-16x16.png
+    ├── favicon-32x32.png
+    ├── msapplication-icon-144x144.png
+    └── mstile-150x150.png
+```
---
+++ resize.py
@@ -0,0 +1,67 @@
+from PIL import Image
+import sys
+import os
+import json
+import traceback
+from datetime import datetime
+
+MANIFEST_PATH = './resize_manifest.json'
+DIST_DIR = './dist'
+
+
+def main(base_image_path):
+    if os.path.exists(base_image_path) is False:
+        print('"{}" does not exists.'.format(base_image_path))
+        return False
+
+    manifest_data = get_manifest_data()
+    if manifest_data is False:
+        return False
+
+    process_ts = str(datetime.now().strftime('%s'))
+    dist_dir = '{}/{}'.format(DIST_DIR, process_ts)
+    os.mkdir(dist_dir)
+    if os.path.exists(dist_dir) is False:
+        print('Failed to create dist directly.')
+        return False
+
+    base_file_object = Image.open(base_image_path)
+    for manifest in manifest_data:
+        resize_image(base_file_object, manifest, dist_dir)
+
+
+def resize_image(base_file_object, manifest, dist_dir):
+    try:
+        img_resize = base_file_object.resize(
+            (manifest['x'], manifest['y']), Image.LANCZOS)
+        img_resize.save('{}/{}'.format(dist_dir, manifest['name']))
+    except Exception:
+        print(traceback.format_exc())
+        print('Failed to resize to "{}" sized {} x {}.'.format(
+            manifest['name'], manifest['x'], manifest['y']))
+        return False
+
+
+def get_manifest_data():
+    if os.path.exists(MANIFEST_PATH) is False:
+        print('"resize_manifest.json" does not exists.')
+        return False
+
+    try:
+        f = open(MANIFEST_PATH, 'r')
+        manifest_data = json.load(f)
+        return manifest_data
+    except Exception:
+        print(traceback.format_exc())
+        print('An error occuerd at loading manifest data.')
+        return False
+
+
+if __name__ == '__main__':
+    args = sys.argv
+    if len(args) < 2:
+        print('First argument for base image file path is required.')
+        quit()
+
+    print(args[1])
+    main(args[1])
